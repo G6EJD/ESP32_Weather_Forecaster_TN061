f9dc82e32d6d0446629656aa71902fe3109c477c
G6EJD <d.l.bird@sky.com>
2020-06-27 08:59:24 +0100

    Improved forecasting algorithm

--- ESP_Zambretti_v3.ino
+++ ESP_Zambretti_v3.ino
@@ -142,7 +142,8 @@ void calc_zambretti(float zpressure, String ztrend, int zmonth, String windDirec
   }
   // FALLING
   if (ztrend == "falling") {
-    zambretti = round((double)0.0000246203 * pow(zpressure,3) - 0.0748992301 * pow(zpressure,2) + 75.8125990475 * zpressure - 25,526.8083073332));
+    zambretti = 0.0000246203 * pow(zpressure,3) - 0.0748992301 * pow(zpressure,2) + 75.8125990475 * zpressure - 25,526.8083073332;
+    zambretti = round(zambretti);
     if (hemiSphere == "northern") 
     {
       if (zmonth < 4 || zmonth > 9) zambretti = zambretti - 1;   // + 1 to adjust the forecast in Northern H. Winter, makes the weather worst!
